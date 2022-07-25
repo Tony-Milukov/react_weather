55fecafe2544a3d8b90b344684f151c65a1b43ce
Tony-Milukov <89128409+Tony-Milukov@users.noreply.github.com>
2022-07-25 17:39:14 +0200

    ADDED THE PROJECT

---
+++ my-app/src/availableCities/avaibleCities.js
@@ -0,0 +1,41 @@
+export const avaibleCities = [
+    {
+        city : "Berlin",
+        name: "Berlin",
+        flag: "germany"
+    },
+    {
+        city : "New%20York",
+        name: "New York",
+        flag: "USA"
+    },
+    {
+        city : "Wien",
+        name: "Wien",
+        flag: "germany"
+    },
+    {
+        city : "Klagenfurt",
+        name: "Klagenfurt",
+        flag: "austria"
+    },
+    {
+        city : "Moskau",
+        name: "Moskow",
+        flag: "russia"
+    },
+
+    {
+        city : "Kiev",
+        name: "Kiev",
+        flag: "Ukraine"
+    },
+    {
+        city : "Mariupol",
+        name: "Mariupol",
+        flag: "Ukraine"
+    },
+
+
+
+]
